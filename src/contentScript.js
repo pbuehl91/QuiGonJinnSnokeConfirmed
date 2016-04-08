8839c82b5d0eb78d233344a1510e18a2fbc4fda1
pbuehl91 <Patrick.Buehl91@gmail.com>
2016-04-08 14:09:30 -0500

    Update contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
    
-   var cageAry = [
-    'http://spooool.com/wp-content/uploads/2011/11/NicCageFace.jpg',
-    'http://upload.wikimedia.org/wikipedia/commons/3/33/Nicolas_Cage_2011_CC.jpg',
-    'http://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29.jpg/170px-Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29.jpg',
+   var jinn = [
+    'http://img.lum.dolimg.com/v1/images/Qui-Gon-Jinn_d89416e8.jpeg?region=0%2C1%2C1536%2C864&width=768',
+    'http://vignette3.wikia.nocookie.net/disney/images/7/7b/Qui-gon_jinn_and_ben_kenobi.jpg/revision/latest?cb=20131030230015',
+    'http://vignette4.wikia.nocookie.net/disney/images/d/de/Qui-gon-jinn.jpg/revision/latest?cb=20131030230457',
     'http://img2.wikia.nocookie.net/__cb20130727161809/feed-the-beast/images/e/e0/Nick-Cage.jpg',
     'http://www.hdwallpapersinn.com/wp-content/uploads/2014/07/Nicolas-Cage-Celebrity-Wallpaper-HD.jpg',
     'http://media1.giphy.com/media/UdqUo8xvEcvgA/200_s.gif'
    ]
    
-   var randomCage = cageAry[Math.floor(Math.random()*cageAry.length)];
+   var jinned = jinn[Math.floor(Math.random()*jinn.length)];
 
-	$('img').attr('src', randomCage );
+	$('img').attr('src', jinned );
 
 	alert("Qui Gon Jinn, Snoke Confirmed!");
 
