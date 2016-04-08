b1ba0ff019e27130d774c1dd4f4934f97ac39301
pbuehl91 <Patrick.Buehl91@gmail.com>
2016-04-08 14:10:38 -0500

    Update contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
     'http://img.lum.dolimg.com/v1/images/Qui-Gon-Jinn_d89416e8.jpeg?region=0%2C1%2C1536%2C864&width=768',
     'http://vignette3.wikia.nocookie.net/disney/images/7/7b/Qui-gon_jinn_and_ben_kenobi.jpg/revision/latest?cb=20131030230015',
     'http://vignette4.wikia.nocookie.net/disney/images/d/de/Qui-gon-jinn.jpg/revision/latest?cb=20131030230457',
-    'http://img2.wikia.nocookie.net/__cb20130727161809/feed-the-beast/images/e/e0/Nick-Cage.jpg',
-    'http://www.hdwallpapersinn.com/wp-content/uploads/2014/07/Nicolas-Cage-Celebrity-Wallpaper-HD.jpg',
-    'http://media1.giphy.com/media/UdqUo8xvEcvgA/200_s.gif'
+    'http://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/54b008afe4b0160e5972de21/1420822704832/',
+    'http://41.media.tumblr.com/e21aae73ca3de8248d7e51e3cfb5725a/tumblr_nwtxy5Y6JZ1uvim24o1_1280.png',
+    'http://theforce.net/episode1/oldpreq/characters/pics/t-quigon2.jpg'
    ]
    
    var jinned = jinn[Math.floor(Math.random()*jinn.length)];
